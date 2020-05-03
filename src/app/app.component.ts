b95945d55c8a3d2bbfdf530f3cefc69e738e4de3
Jairo-desktop <jairofontalvo@hotmail.es>
2020-05-03 13:29:04 -0500

    Calculadora refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,7 @@ export class AppComponent {
   initial = '';
 
   //Metodo para leer la tecla que se pulsa en la calculadora
-  clickNumber(n) {
-    console.log(n);
+  clickNumber( n ) {
       if (this.numCurrent === '0') {
         this.numCurrent = n
       } else {
@@ -26,6 +25,7 @@ export class AppComponent {
   insertDecimal() {
       if (!this.numCurrent.includes('.')) {
           this.numCurrent += '.';
+          this.decState = false;
       } else {
           if (this.decState) {
               this.numCurrent += '.';
@@ -35,37 +35,39 @@ export class AppComponent {
   }
 
   //Metodo leer la operacion que se pulsa en la calculadora
-  getOperation(o) {
-      if (o == "=") {
+  getOperation( o ) {
+      if ( o == "=" ) {
         // en const x capturo el ultimo x numero ingresado
-          const x = this.numCurrent.charAt(this.numCurrent.length - 1);
+          const x = this.numCurrent.charAt( this.numCurrent.length - 1 );
           var re = new RegExp("([0-9])");
 
           //resultado del test donde ultimo x numero es evaluado
-          if (re.test(x)) {
+          if ( re.test(x) ) {
               const result = eval(this.numCurrent);
               this.initial = this.numCurrent;
               this.numCurrent = String(result);
-
-              console.log(this.numCurrent);
-
           } else {
               //Si lo presionado no fue un numero, se alerta de un error
-              alert("Error. indique un segundo numero despues de la operacion");
+              alert("Error! Enter a second number after the operation");
               this.clean();
           }
 
-      }else{
+      } else {
           //Si operacion no es '='. Concatene segun la operacion y cambie el estado de decState a true
-          this.numCurrent += o
-          this.decState = true;
+            var numUlt = this.numCurrent.charAt( this.numCurrent.length - 1 );
+
+            if( !(numUlt == "+" || numUlt == "-" || numUlt == "*" || numUlt == "/" ) ) {
+                this.numCurrent += o;
+                this.decState = true;
+            }
       }
   }
 
-  //Metodo para restablecer las variables
+  //Metodo para restablecer las variables a su estado inicial
   clean() {
       this.numCurrent = '0';
       this.initial = '';
       this.numState = false;
+      this.decState = false;
   }
 }
